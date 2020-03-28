fa5d521f47fd97fac81414f7e743a90642349a5d
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2020-03-28 12:28:05 -0700

    chore: improve naming and comments
    - use more descriptive naming.
    - add comments.
    - move APDU flag constants to an enum.
    - increase APDU Payload max from 4 to 10 chunks.
    - add more argument checks.
    - use linting.
    
    Changes are non-breaking.

--- src/contracts.ts
+++ src/contracts.ts
@@ -1,6 +1,6 @@
 export interface Transport {
-	getPublicKey(path: string): Promise<string>,
-	signTransaction(path: string, hex: Buffer): Promise<string>,
-	signMessage(path: string, hex: Buffer): Promise<string>,
-	getVersion(): Promise<string>
+	getPublicKey(path: string): Promise<string>;
+	getVersion(): Promise<string>;
+	signTransaction(path: string, hex: Buffer): Promise<string>;
+	signMessage(path: string, hex: Buffer): Promise<string>;
 }
