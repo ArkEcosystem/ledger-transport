843260bcb5da490f24ad36c81576f3e748c4eb5a
Simon <sleepdefic1t@users.noreply.github.com>
2021-02-14 11:48:00 -0500

    chore: remove ecdsa support (#50)

--- __tests__/ark.test.ts
+++ __tests__/ark.test.ts
@@ -34,29 +34,6 @@ describe("ARKTransport", () => {
             const ark = await TransportMock.getARKTransport(RecordStore.fromString(Fixtures.publicKey.record));
             await expect(ark.getPublicKey(Fixtures.bip44.path.valid)).resolves.toEqual(Fixtures.publicKey.result);
         });
-
-        it("should pass with a compressed publicKey for ark ledger app <= 2.0.1", async () => {
-            const ark = await TransportMock.getARKTransport(RecordStore.fromString(Fixtures.publicKey.legacy.record));
-            await expect(ark.getPublicKey(Fixtures.bip44.path.valid)).resolves.toEqual(Fixtures.publicKey.result);
-        });
-    });
-
-    describe("signMessage", () => {
-        it("should pass with an ecdsa signature", async () => {
-            const ark = await TransportMock.getARKTransport(RecordStore.fromString(Fixtures.message.ecdsa.record));
-            const message = Buffer.from(Fixtures.message.ecdsa.payload, "hex");
-            await expect(ark.signMessage(Fixtures.bip44.path.valid, message)).resolves.toEqual(
-                Fixtures.message.ecdsa.result,
-            );
-        });
-
-        it("should throw with a message containing non-ascii characters", async () => {
-            const ark = await TransportMock.getARKTransport(RecordStore.fromString(Fixtures.message.ecdsa.record));
-            const message = Buffer.from(Fixtures.message.invalid, "utf-8");
-            await expect(ark.signMessage(Fixtures.bip44.path.valid, message)).rejects.toThrow(
-                TransportErrors.MessageAsciiError,
-            );
-        });
     });
 
     describe("signMessageWithSchnorr", () => {
@@ -67,14 +44,12 @@ describe("ARKTransport", () => {
                 Fixtures.message.schnorr.result,
             );
         });
-    });
 
-    describe("signTransaction", () => {
-        it("should pass with an ecdsa signature", async () => {
-            const ark = await TransportMock.getARKTransport(RecordStore.fromString(Fixtures.transaction.ecdsa.record));
-            const transaction = Buffer.from(Fixtures.transaction.ecdsa.payload, "hex");
-            await expect(ark.signTransaction(Fixtures.bip44.path.valid, transaction)).resolves.toEqual(
-                Fixtures.transaction.ecdsa.result,
+        it("should throw with a message containing non-ascii characters", async () => {
+            const ark = await TransportMock.getARKTransport(RecordStore.fromString(Fixtures.message.schnorr.record));
+            const message = Buffer.from(Fixtures.message.invalid, "utf-8");
+            await expect(ark.signMessageWithSchnorr(Fixtures.bip44.path.valid, message)).rejects.toThrow(
+                TransportErrors.MessageAsciiError,
             );
         });
     });
